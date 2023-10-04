467abe849de454a7d4dab7de4250613c8c61df71
Ruslan <rus05vorobjev@example.com>
2023-10-04 19:24:04 +0300

    Создал метод для ввода элементов массива

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.Write("Введите количество элементов массива: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+string [] Arr = new string [number];
+void array(string [] Arr)
+{
+  for (int i = 0;i<Arr.Length;i++)
+  {
+     Console.WriteLine($"Введите {i+1} элемент массива");
+     Arr[i] = Console.ReadLine();
+  }
+}
